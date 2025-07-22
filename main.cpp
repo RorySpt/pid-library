744d5cd150a8d47d7904c8a4769b6c6a22c015dc
zhanglinlin <zhanglinlin689@outlook.com>
2025-07-22 17:45:39 +0800

    临时提交

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <fmt/base.h>
-
+#include <imgui.h>
 #include "pid.h"
 
 
 int main() {
+
     constexpr pid::CascadePID::Params inParams = {1.0, 0.1, 0.01, 100.0, 100.0};
     constexpr pid::CascadePID::Params outParams = {0.5, 0.05, 0.005, 50.0, 50.0};
 
 
 
-    pid::CascadePID pid(inParams, outParams);
+    pid::PID pid(inParams);
     for (int i = 0; i < 100; ++i) {
-        pid.calc(10.0, 5.0, 2.0);
+        pid.calc(5.0, 2.0);
         fmt::println("out_put {}", pid.getOutput());
     }
 
